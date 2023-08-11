391acb570988cf7d32405b48479d0f91da4c6cc0
zlx <1104175963@qq.com>
2023-08-11 15:56:05 +0800

    1

--- src/api/request.js
+++ src/api/request.js
@@ -13,7 +13,7 @@ axios.defaults.headers['Content-Type'] = 'application/json;charset=utf-8'
 // 创建axios实例
 const service = axios.create({
   // axios中请求配置有baseURL选项，表示请求URL公共部分
-  baseURL: process.env.NODE_ENV=='development'?'http://192.168.196.37:8080/': window.location.origin,
+  baseURL: process.env.NODE_ENV=='development'?'http://192.168.196.37:8080/': 'http://172.16.0.4:8088/',
   // 超时
   timeout: 10000
 })
