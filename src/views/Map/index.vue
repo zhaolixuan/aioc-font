812651dddd7be6e0320e4fdb93fda70c27be7e25
zhaolixuan <1104175963@qq.com>
2024-06-05 11:26:33 +0800

    先开头添加标记

--- src/views/Map/index.vue
+++ src/views/Map/index.vue
@@ -11,8 +11,8 @@
     ></information-panel>
   </div>
 </template>
-   
-  <script>
+
+<script>
 import api from "@/api/api";
 import AMapLoader from "@amap/amap-jsapi-loader";
 import { debounce, throttle } from "loadsh";
@@ -21,22 +21,22 @@ import InformationPanel from "./Panel/InformationPanel";
 import { obtainZone } from "@/utils/pointiInZone";
 
 window._AMapSecurityConfig = {
-  securityJsCode: "83d32f3ed10df19e1715137b322f990b", //你的安全密钥
+  securityJsCode: "83d32f3ed10df19e1715137b322f990b" //你的安全密钥
 };
 export default {
   name: "Map",
   props: {
     mapCenter: {
       type: String,
-      default: "116.38821589024599|39.97533303800978",
+      default: "116.38821589024599|39.97533303800978"
     },
     zoom: {
       type: Number,
-      default: 13,
-    },
+      default: 13
+    }
   },
   components: {
-    InformationPanel,
+    InformationPanel
   },
   data() {
     return {
@@ -50,20 +50,20 @@ export default {
       myDataList: [],
       infoWindow: null,
       optionSelectAlarmType: null,
-      zoneList: [],
+      zoneList: []
     };
   },
   watch: {
     mapCenter: {
       deep: true,
-      handler: function (data) {
+      handler: function(data) {
         // console.log(data);
         if (!data) return;
         if (this.map) {
           this.map.panTo(data.split("|"), 500);
         }
-      },
-    },
+      }
+    }
   },
   async mounted() {
     this.initMap();
@@ -72,7 +72,7 @@ export default {
   methods: {
     handelgive(data = {}) {
       let arr = obtainZone(data, this.zoneList);
-      arr.forEach((i) => {
+      arr.forEach(i => {
         this.getliner(i.id, data);
       });
     },
@@ -88,32 +88,32 @@ export default {
           "AMap.PolyEdito",
           "AMap.MouseTool",
           "AMap.Autocomplete",
-          "AMap.PlaceSearch",
-        ], // 需要使用的的插件列表，如比例尺'AMap.Scale'等
-      }).then((AMap) => {
+          "AMap.PlaceSearch"
+        ] // 需要使用的的插件列表，如比例尺'AMap.Scale'等
+      }).then(AMap => {
         this.map = new AMap.Map("map", {
           zoom: this.zoom,
           center: this.mapCenter && this.mapCenter.split("|"),
           mapStyle: "amap://styles/darkblue",
-          showIndoorMap: false,
+          showIndoorMap: false
         });
         window.map = this.map;
         this.zhujiicon = new AMap.Icon({
           size: new AMap.Size(32, 32), // 设置图标大小
           image: require(`../../assets/image/zhuji.png`), // 设置图标路径
-          imageSize: new AMap.Size(32, 32), // 设置图标显示大小
+          imageSize: new AMap.Size(32, 32) // 设置图标显示大小
         });
         this.waisheicon = new AMap.Icon({
           size: new AMap.Size(32, 32), // 设置图标大小
           image: require(`../../assets/image/waishe.png`), // 设置图标路径
-          imageSize: new AMap.Size(32, 32), // 设置图标显示大小
+          imageSize: new AMap.Size(32, 32) // 设置图标显示大小
         });
 
         let el = document.getElementById("informationPanel");
         this.infoWindow = new AMap.InfoWindow({
           isCustom: true,
           content: el,
-          offset: new AMap.Pixel(0, -30),
+          offset: new AMap.Pixel(0, -30)
         });
 
         this.getMarkerData();
@@ -122,11 +122,11 @@ export default {
     },
     getMarkerData() {
       this.marker = [];
-      api.aideDeviceList().then((res) => {
+      api.aideDeviceList().then(res => {
         this.addMaker(res.rows, "waishe");
       });
 
-      api.hostManageList().then((res) => {
+      api.hostManageList().then(res => {
         this.addMaker(res.rows, "zhuji");
       });
     },
@@ -145,8 +145,9 @@ export default {
           const marker = new AMap.Marker({
             icon: this[`${type}icon`],
             position: ba_gd(lnglat),
-            anchor: "bottom-center",
+            anchor: "center",
             id: item.aideDeviceId,
+            // offset: new AMap.Pixel(0, 0), //以 icon 的 [center bottom] 为原点
             extData: {
               name: item.hostName,
               labelName: item.hostName,
@@ -154,19 +155,19 @@ export default {
               hostId: item.hostId || "",
               channelName: item.channelName || "",
               channelZoneName: item.channelZoneName || "0",
-              ip: item.ip || "",
-            },
+              ip: item.ip || ""
+            }
           })
             .on(
               "mouseout",
-              debounce(function () {
+              debounce(function() {
                 // console.log("mack移除");
                 _this.isShow = false;
                 _this.infoWindow.close();
               }),
               200
             )
-            .on("mouseover", function (e) {
+            .on("mouseover", function(e) {
               let properties = e.target.getExtData();
               _this.isShow = true;
               _this.myTitleName = properties.name;
@@ -175,27 +176,27 @@ export default {
                 _this.myDataList = [
                   {
                     fieldName: "主机ID",
-                    value: properties.hostId,
+                    value: properties.hostId
                   },
                   {
                     fieldName: "所属地区",
-                    value: properties.region,
-                  },
+                    value: properties.region
+                  }
                 ];
               } else {
                 _this.myDataList = [
                   {
                     fieldName: "所属通道",
-                    value: properties.channelName,
+                    value: properties.channelName
                   },
                   {
                     fieldName: "所属分区",
-                    value: properties.channelZoneName,
+                    value: properties.channelZoneName
                   },
                   {
                     fieldName: "ip地址",
-                    value: properties.ip,
-                  },
+                    value: properties.ip
+                  }
                 ];
               }
               _this.infoWindow.open(_this.map, e.target.getPosition());
@@ -208,16 +209,20 @@ export default {
     addline() {
       this.lines = [];
       var _this = this;
-      api.zoneList().then((res) => {
-        // console.log('res',res);
+      api.zoneList().then(res => {
         if (!res.rows.length) return;
         this.zoneList = res.rows;
-        res.rows.forEach((item) => {
+        const qipaoicon = new AMap.Icon({
+          size: new AMap.Size(35, 30), // 设置图标大小
+          image: require(`../../assets/image/qipao.png`), // 设置图标路径
+          imageSize: new AMap.Size(35, 30) // 设置图标显示大小
+        });
+        res.rows.forEach(item => {
           if (item.latscope) {
             var reg = new RegExp("\n", "g");
             let resData = item.latscope.replace(reg, "").split(",");
             let pointData = [];
-            resData.forEach((j) => {
+            resData.forEach(j => {
               let resData_item = ba_gd(j.split("|"));
               if (resData_item.length) {
                 pointData.push([resData_item[0] * 1, resData_item[1] * 1]);
@@ -233,37 +238,48 @@ export default {
                 channelName: item.channelName,
                 channelEndNum: item.channelEndNum,
                 pointDataStart: pointData[0],
-                pointDataEnd: pointData[pointData.length - 1],
-              },
+                pointDataEnd: pointData[pointData.length - 1]
+              }
             })
               .on(
                 "mouseout",
-                debounce(function () {
+                debounce(function() {
                   // console.log("line移除");
                   _this.infoWindow.close();
                   _this.isShow = false;
                 }, 200)
               )
-              .on("mouseover", function (e) {
+              .on("mouseover", function(e) {
                 let properties = e.target.getExtData();
                 _this.isShow = true;
                 _this.myTitleName = properties.name;
                 _this.myDataList = [
                   {
                     fieldName: "所属通道",
-                    value: properties.channelName,
+                    value: properties.channelName
                   },
                   {
                     fieldName: "分区起点：",
-                    value: properties.channelStartNum,
+                    value: properties.channelStartNum
                   },
                   {
                     fieldName: "分区终点：",
-                    value: properties.channelEndNum,
-                  },
+                    value: properties.channelEndNum
+                  }
                 ];
                 _this.infoWindow.open(_this.map, [e.lnglat.lng, e.lnglat.lat]);
               });
+              console.log(item);
+            const position = new AMap.LngLat(pointData[0][0], pointData[0][1]);
+            // const markerContent = `<div class="custom-content-marker">${item.channelName}</div>`;
+            const marker = new AMap.Marker({
+              icon: qipaoicon,
+              position: position,
+              label: { content: item.channelZoneName, direction: "center",fontsize:'12px' }, //将 html 传给 content
+              offset: new AMap.Pixel(0, -30) //以 icon 的 [center bottom] 为原点
+            });
+            _this.map.add(marker);
+
             _this.map.add(polyline);
             _this.lines.push({ id: item.channelZoneId, polyline: polyline });
           }
@@ -273,7 +289,7 @@ export default {
 
     getliner(id, data) {
       let _this = this;
-      let targetPolyline = this.lines.find((i) => i.id == id);
+      let targetPolyline = this.lines.find(i => i.id == id);
       if (targetPolyline) {
         targetPolyline.polyline.setOptions({
           strokeColor: "red",
@@ -282,27 +298,26 @@ export default {
             channelStartNum: data.startPosition,
             channelEndNum: data.endPosition,
             alarmType: this.getAlarmLabel(data.alarmType),
-            name:data.fenquName
-          },
+            name: data.fenquName
+          }
         });
-        targetPolyline.polyline.on("mouseover", function (e) {
+        targetPolyline.polyline.on("mouseover", function(e) {
           let properties = e.target.getExtData();
           _this.isShow = true;
           _this.myTitleName = properties.name;
           _this.myDataList = [
             {
               fieldName: "报警分区：",
-              value: properties.name,
+              value: properties.name
             },
             {
               fieldName: "报警点：",
-              value:
-                properties.channelStartNum + "-" + properties.channelEndNum,
+              value: properties.channelStartNum + "-" + properties.channelEndNum
             },
             {
               fieldName: "报警类型",
-              value: properties.alarmType,
-            },
+              value: properties.alarmType
+            }
           ];
           _this.infoWindow.open(_this.map, [e.lnglat.lng, e.lnglat.lat]);
         });
@@ -318,15 +333,15 @@ export default {
       }
     },
     setOptionAlarms() {
-      api.optionsAlarmType().then((response) => {
+      api.optionsAlarmType().then(response => {
         this.optionSelectAlarmType = response.data;
       });
-    },
-  },
+    }
+  }
 };
 </script>
-   
-  <style scoped>
+
+<style scoped>
 #map {
   width: 100%;
   height: 100%;
@@ -343,5 +358,20 @@ export default {
   pointer-events: none;
   background-image: url("~@/assets/image/map-mask.png");
   background-size: 100% 100%;
+}
+</style>
+<style>
+.amap-marker-label {
+  background: transparent;
+  border: none;
+  color: #fff;
+  width: 32px;
+  height: 32px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-wrap: wrap;
+  white-space: break-spaces;
+  font-size: 10px;
 }
 </style>
